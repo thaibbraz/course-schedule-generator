8db06f05d642daa478fcada70c1a168ae31c90c9
Thainá <thainabbraz@gmail.com>
2022-05-14 16:29:47 +0200

    PM course added

---
+++ arrayGenerator.py
@@ -0,0 +1,292 @@
+def get_full_stack_part_time():
+
+    return [
+            {
+            "Week": "Week 1",
+            "week_day":["Monday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Introduction & Variables","Conditionals"]
+        },{
+            "Week": "Week 2",
+            "week_day":["Monday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Objects","Arrays"]
+        },{
+            "Week": "Week 3",
+            "week_day":["Monday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Loops","Looping through Objects"]
+        },{
+            "Week": "Week 4",
+            "week_day":["Monday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Looping through Arrays","Functions"]
+        },{
+            "Week": "Week 5",
+            "week_day":["Monday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Higher Order Functions","Closures", "Milestone 1"]
+        },{
+            "Week": "Week 6",
+            "week_day":["Monday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Classes & Testing and File Structure","Queues and Stacks"]
+        },{
+            "Week": "Week 7",
+            "week_day":["Monday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Linked Lists","Recursion", "Milestone 2"]
+        },{
+            "Week": "Week 8",
+            "week_day":["Monday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Trees", "Graphs"]
+        },{
+            "Week": "Week 9",
+            "week_day":["Monday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Manipulating the DOM","Manipulating the DOM (continued)", "Milestone 3"]
+        },{
+            "Week": "Week 10",
+            "week_day":["Monday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Vue","Vue"]
+        },{
+            "Week": "Week 11",
+            "week_day":["Monday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Vue","Vue"]
+        },   {
+            "Week": "Week 12",
+            "week_day":["Monday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["React","React"]
+        },{
+            "Week": "Week 13",
+            "week_day":["Monday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["React","React", "Milestone 4"]
+        },{
+            "Week": "Week 14",
+            "week_day":["Monday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Fetch", "APIs"]
+        },{
+            "Week": "Week 15",
+            "week_day":["Monday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Databases","Databases", "Milestone 5"]
+        },{
+            "Week": "Week 16",
+            "week_day":["Monday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["MVP (Start Solo Project )","MVP"]
+        },{
+            "Week": "Week 17",
+            "week_day":["Monday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["MVP","MVP"]
+        },{
+            "Week": "Week 18",
+            "week_day":["Monday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["MVP","MVP"]
+        },{
+            "Week": "Week 19",
+            "week_day":["Monday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["MVP Presentation","Start Feature Extension"]
+        },{
+            "Week": "Week 20",
+            "week_day":["Monday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Feature Extension","Feature Extension"]
+        },{
+            "Week": "Week 21",
+            "week_day":["Monday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Feature Extension Presentation","Start Final Project"]
+        },{
+            "Week": "Week 22",
+            "week_day":["Monday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Final Project","Final Project"]
+        },{
+            "Week": "Week 23",
+            "week_day":["Monday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Final Project","Final Project"]
+        },{
+            "Week": "Week 24",
+            "week_day":["Monday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Final Project","Final Project Presentation"]
+        },{
+            "Week": "Week 25",
+            "week_day":["Monday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Career Prep","Career Prep"]
+        },{
+            "Week": "Week 24",
+            "week_day":["Monday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Career Prep","Career Prep & Graduation"]
+        }
+
+    ]
+
+def get_full_stack_full_time():
+    return  [
+            {
+            "Week": "Week 1",
+            "week_day": ["Tuesday", "Thursday", "Wednesday", "Thursday", "Friday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Introduction, Variables, Operators, and Conditionals","Objects & Arrays","Loops & Looping through Objects","Looping through Arrays & Functions", "Milestone 1"]
+        },{
+            "Week": "Week 2",
+            "week_day": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Higher Order Functions & Closures","Classes, Testing, & File Structure","Queues and Stacks & Linked Lists", "Linked List & Recursion", "Milestone 2"]
+        },{
+            "Week": "Week 3",
+            "week_day": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Trees","Graphs","Manipulating the DOM","Manipulating the DOM (continued)", "Milestone 3"]
+        },{
+            "Week": "Week 4",
+            "week_day": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Vue","Vue (continued)","React","React (continued)", "Milestone 4"]
+        },{
+            "Week": "Week 5",
+            "week_day": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Fetch","APIs","Databases", "Databases (continued)", "Milestone 5"]
+        },{
+            "Week": "Week 6",
+            "week_day": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["MVP Solo Project","MVP Solo Project","MVP Solo Project", "MVP Solo Project","MVP Solo Project"]
+        },{
+            "Week": "Week 7",
+            "week_day": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["MVP Solo Project","MVP Solo Project","Feature Extension Project", "Feature Extension Project","Feature Extension Project"]
+        },{
+            "Week": "Week 8",
+            "week_day": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Feature Extension Project","Feature Extension Project","Feature Extension Project Presentation", "Collaborative Project","Collaborative Project"]
+        },{
+            "Week": "Week 9",
+            "week_day": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Collaborative Project","Collaborative Project","Collaborative Project", "Collaborative Project","Collaborative Project"]
+        },{
+            "Week": "Week 10",
+            "week_day": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Collaborative Project","Collaborative Project","Collaborative Project", "Collaborative Project","Collaborative Project"]
+        },{
+            "Week": "Week 11",
+            "week_day": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Collaborative Project Presentation & Career Week","Career Week","Career Week", "Career Week","Graduation"]
+        },
+
+    ]
+def get_product_management():
+    return  [
+            {
+            "Week": "Week 1",
+            "week_day": ["Tuesday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Intro & Role of the PM","Role of the PM"]
+        },{
+            "Week": "Week 2",
+            "week_day": ["Tuesday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Creating a Hypothesis","Creating a Hypothesis"]
+        },{
+            "Week": "Week 3",
+            "week_day": ["Tuesday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Testing a Hypothesis ","Testing a Hypothesis"]
+        },{
+            "Week": "Week 4",
+            "week_day": ["Tuesday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Scoping your MVP","Scoping your MVP"]
+        },{
+            "Week": "Week 5",
+            "week_day": ["Tuesday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Scoping your MVP", "Scoping your MVP"]
+        },{
+            "Week": "Week 6",
+            "week_day": ["Tuesday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Design and Delivery", "Design and Delivery"]
+        },{
+            "Week": "Week 7",
+            "week_day": ["Tuesday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Leveraging Analytics for Product Reviews", "JIRA + Confluence + UX Tools + Mapping Tools (Miro type)"]
+        },{
+            "Week": "Week 8",
+            "week_day": ["Tuesday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Launch Strategies & Review", "Sharpening Skills"]
+        },{
+            "Week": "Week 9",
+            "week_day": ["Tuesday", "Thursday"],
+            "date": [],
+            "sentence": [],
+            "class_title": ["Conflict management & career tips","Final Presentations"]
+        }
+    ]
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from rw_sheet import read_sheets, write_final_result
 import pandas as pd
 import holidays
 from datetime import date
+import time
 SPREADSHEET_ID = '12nqjSOKMlWhhbg6F6yRWVaKU00qq2nr-pCcxFmz4cGg'
-
+from arrayGenerator import get_full_stack_part_time,get_full_stack_full_time,get_product_management
 
 where_to_write = "Sheet1!A1"
 where_to_read = where_to_write
@@ -13,119 +14,61 @@ where_to_read = where_to_write
 es_holidays = holidays.Spain()
 holiday_names = holidays.country_holidays('ES', subdiv='CT')
 
-full_stack_full_time = [
-        {
-        "Week": "Week 1",
-        "week_day": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
-        "date": [],
-        "sentence": [],
-        "class_title": ["Introduction, Variables, Operators, and Conditionals","Objects & Arrays","Loops & Looping through Objects","Looping through Arrays & Functions", "Milestone 1"]
-    },{
-        "Week": "Week 2",
-        "week_day": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
-        "date": [],
-        "sentence": [],
-        "class_title": ["Higher Order Functions & Closures","Classes, Testing, & File Structure","Queues and Stacks & Linked Lists", "Linked List & Recursion", "Milestone 2"]
-    },{
-        "Week": "Week 3",
-        "week_day": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
-        "date": [],
-        "sentence": [],
-        "class_title": ["Trees","Graphs","Manipulating the DOM","Manipulating the DOM (continued)", "Milestone 3"]
-    },{
-        "Week": "Week 4",
-        "week_day": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
-        "date": [],
-        "sentence": [],
-        "class_title": ["Vue","Vue (continued)","React","React (continued)", "Milestone 4"]
-    },{
-        "Week": "Week 5",
-        "week_day": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
-        "date": [],
-        "sentence": [],
-        "class_title": ["Fetch","APIs","Databases", "Databases (continued)", "Milestone 5"]
-    },{
-        "Week": "Week 6",
-        "week_day": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
-        "date": [],
-        "sentence": [],
-        "class_title": ["MVP Solo Project","MVP Solo Project","MVP Solo Project", "MVP Solo Project","MVP Solo Project"]
-    },{
-        "Week": "Week 7",
-        "week_day": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
-        "date": [],
-        "sentence": [],
-        "class_title": ["MVP Solo Project","MVP Solo Project","Feature Extension Project", "Feature Extension Project","Feature Extension Project"]
-    },{
-        "Week": "Week 8",
-        "week_day": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
-        "date": [],
-        "sentence": [],
-        "class_title": ["Feature Extension Project","Feature Extension Project","Feature Extension Project Presentation", "Collaborative Project","Collaborative Project"]
-    },{
-        "Week": "Week 9",
-        "week_day": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
-        "date": [],
-        "sentence": [],
-        "class_title": ["Collaborative Project","Collaborative Project","Collaborative Project", "Collaborative Project","Collaborative Project"]
-    },{
-        "Week": "Week 10",
-        "week_day": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
-        "date": [],
-        "sentence": [],
-        "class_title": ["Collaborative Project","Collaborative Project","Collaborative Project", "Collaborative Project","Collaborative Project"]
-    },{
-        "Week": "Week 11",
-        "week_day": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
-        "date": [],
-        "sentence": [],
-        "class_title": ["Collaborative Project Presentation & Career Week","Career Week","Career Week", "Career Week","Graduation"]
-    },
 
-]
 
-# start_date = input("What's the date the course will start? FORMAT: '2/24/2020' ")
-# end_date = input("What's the date the course will end? FORMAT: '2/24/2020' ")
+start_date = input("What's the date the course will start? FORMAT: '2/24/2020' ")
+end_date = input("What's the date the course will end? FORMAT: '2/24/2020' ")
 
-# if start_date == end_date:
-#     print("The course will be on the same day")
-# else:
-
+cohort = input("What's the cohort?  FORMAT: 'FSPT' or 'FSFT' ")
+if cohort == "FSPT":
+    cohort_list = get_full_stack_part_time()
+elif cohort == "FSFT":
+    cohort_list = get_full_stack_full_time()
+elif cohort == "PM":
+    cohort_list = get_product_management()
 
 #Filter Holidays
 holidays_list = []
-for item in pd.bdate_range(start="4/18/2022", end="12/19/2022",freq='B'):
+if cohort == "FSPT":
+     dates_list = pd.bdate_range(start=start_date, end=end_date,freq='C',weekmask='Mon Thu')
+elif cohort == "FSFT":
+     dates_list = pd.bdate_range(start=start_date, end=end_date,freq='B')
+elif cohort == "PM":
+    dates_list = pd.bdate_range(start=start_date, end=end_date,freq='C',weekmask='Tue Thu')
+for item in dates_list:
     if holiday_names.get(item.strftime("%m/%d/%Y")) != None:        
         holidays_list.append(item.strftime("%A %B %d")+" HOLIDAY")
     else:
         holidays_list.append(item.strftime("%A %B %d"))
         # full_stack_full_time[i]["date"].append(pd.bdate_range(start=start_date, end=end_date, freq='B')[counter_date].strftime("%A %B %d"))
-print(holidays_list)
+
 # pd.bdate_range(start=start_date, end=end_date, freq='B')
 counter_date = 0
-for i in range(len(full_stack_full_time)):
-    for j in range(len(full_stack_full_time[i]["week_day"])):
-        full_stack_full_time[i]["date"].append(holidays_list[counter_date])
+full_stack_full_time = get_full_stack_full_time()
+for i in range(len(cohort_list)):
+    for j in range(len(cohort_list[i]["week_day"])):
+        cohort_list[i]["date"].append(holidays_list[counter_date])  
         
         counter_date += 1
                 
 
 counter = 1
-
+print(cohort_list)
 # Check if there's already something written in the sheet
 while read_sheets(where_to_read,SPREADSHEET_ID) != []:
         counter += 1
         where_to_read = "Sheet1!A{counter}".format(counter=counter)             
         where_to_write = where_to_read 
 
-for i in range(len(full_stack_full_time)):
-    for j in range(len(full_stack_full_time[i]["week_day"])):
-        full_stack_full_time[i]["sentence"].append(full_stack_full_time[i]["date"][j] +": "+ full_stack_full_time[i]["class_title"][j])
+for i in range(len(cohort_list)):
+    for j in range(len(cohort_list[i]["week_day"])):
+        cohort_list[i]["sentence"].append(cohort_list[i]["date"][j] +": "+ cohort_list[i]["class_title"][j])
 
 # print(full_stack_full_time[0])
-for item in full_stack_full_time:
+for item in cohort_list:
     write_final_result([[item["Week"]]], "Sheet1!A{counter}".format(counter=counter), SPREADSHEET_ID)
     counter += 1
+    time.sleep(2.4)
     for j in range(len(item["week_day"])):
         counter += 1
         write_final_result([[item["sentence"][j]]], "Sheet1!A{counter}".format(counter=counter), SPREADSHEET_ID)
